15d8a2a1460335a8faea38b7632d13732b12f821
doudou <douoai@163.com>
2022-09-04 16:28:26 +0800

    "新增了插入排序的实现"

---
+++ algorithm/src/cn/doudou/algorithm/sort/Insertion.java
@@ -0,0 +1,41 @@
+package cn.doudou.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * Create By 王嘉浩
+ * Time 2022-09-04 15:57
+ * <p>
+ * 插入排序的实现
+ */
+public class Insertion {
+
+    public static void main(String[] args) {
+        Integer[] arr = {2, 3, 1, 4, 5, 7, 6, 8, 10, 9};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    public static void sort(Comparable[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (greater(arr[j - 1], arr[j])) {
+                    exch(arr, j - 1, j);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static boolean greater(Comparable v, Comparable w) {
+        return v.compareTo(w) > 0;
+    }
+
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
--- algorithm/src/cn/doudou/algorithm/sort/Selection.java
+++ algorithm/src/cn/doudou/algorithm/sort/Selection.java
@@ -5,10 +5,75 @@ import java.util.Arrays;
 /**
  * Create By 王嘉浩
  * Time 2022-09-03 22:30
+ *
+ * 选择排序的实现
+ *
  */
 public class Selection {
     public static void main(String[] args) {
-        int[] arr = {2, 3, 1, 4, 5, 7, 6, 8, 10, 9};
+        long l1 = System.currentTimeMillis();
+        Integer[] arr = {2, 3, 1, 4, 5, 7, 6, 8, 10, 9};
+        Comparable[] sort = sort(arr);
+        System.out.println(Arrays.toString(sort));
+        long l2 = System.currentTimeMillis();
+        System.out.println(l2 - l1);
+    }
+
+    /**
+     * 对数组进行排序的操作
+     *
+     * @param arr
+     * @return
+     */
+    public static Comparable[] sort(Comparable[] arr) {
+        for (int i = 0; i <= arr.length - 2; i++) {
+            //假设i处就是最小值的索引
+            int minIndex = i;
+            //控制比较的后面的那位数
+            for (int j = i + 1; j < arr.length; j++) {
+                //如果后面的数小于预设最小索引的那个数字那么就交换2个数字的位置就可以
+                if (greater(arr[minIndex], arr[j])) {
+                    minIndex = j;
+                }
+            }
+            exch(arr, minIndex, i);
+        }
+        return arr;
+    }
+
+    /**
+     * 比较2个数字的大小
+     *
+     * @param v
+     * @param w
+     * @return
+     */
+    public static boolean greater(Comparable v, Comparable w) {
+        return v.compareTo(w) > 0;
+    }
+
+    /**
+     * 交换数组的位置
+     *
+     * @param a
+     * @param i
+     * @param j
+     */
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+
+    /**
+     * 普通版选择排序的实现过程
+     *
+     * @param arr
+     * @return
+     */
+    public int[] ordinary(int[] arr) {
+        //int[] arr = {2, 3, 1, 4, 5, 7, 6, 8, 10, 9};
         //循环的次数为i-2次就可以
         for (int i = 0; i <= arr.length - 2; i++) {
             //假设i处就是最小值的索引
@@ -23,7 +88,7 @@ public class Selection {
                 }
             }
         }
-        //输出数组
-        System.out.println(Arrays.toString(arr));
+        //返回数组
+        return arr;
     }
 }
